255656ed64d3528e04315f2f4faab4922b86a609
tonymcaron <tonymcaron@gmail.com>
2025-08-11 16:29:33 -0700

    Wrote main-view.jsx code

---
+++ src/main-view/main-view.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: ObjectId('687e7c959tc7839eeeec4ad'),
+      title: "Knocked Up",
+      description: "A one-night stand results in an unexpected pregnancy for a slacker and an up-and-coming media personality.",
+      genre: "Comedy",
+      director: "Judd Apatow",
+      imagePath: "knockedup.png"
+    },
+    {
+      id: ObjectId('687ac9fde77f2b47adeec4a9'),
+      title: "Silence of the Lambs",
+      description: "A young FBI cadet must recieve the help of an incarcerated and manipulative cannibal killer in order to catch another serial killer.",
+      genre: "Thriller",
+      director: "Jonathan Demme",
+      imagePath: "silenceofthelambs.png"
+    },
+    {
+      id: ObjectId('687e7c8995fc7839eeeec4ac'),
+      title: "Rachel Getting Married",
+      description: "A young woman is released from rehab to attend her sister’s wedding and brings chaos with her.",
+      genre: "Drama",
+      director: "Jonathan Demme",
+      imagePath: "rachelgettingmarried.png"
+    }
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  )
+}
