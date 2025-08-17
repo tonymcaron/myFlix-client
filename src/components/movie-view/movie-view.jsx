9d633bbcf0af01c8e59d4b8b09ff13757a4e5a32
tonymcaron <tonymcaron@gmail.com>
2025-08-17 10:06:44 -0700

    Fixed imagePath in movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,7 +2,7 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src="{movie.imagePath}" />
+        <img src={movie.imagePath} />
       </div>
       <div>
         <span>Title: </span>
